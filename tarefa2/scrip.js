42e258f98129b0df19ee134f76045536c5fe4cb9
lino1997 <56599402+lino1997@users.noreply.github.com>
2020-01-09 13:44:16 +0100

    Add files via upload

---
+++ tarefa2/scrip.js
@@ -0,0 +1,19 @@
+/* 
+	segunda tarefa
+	Nome do ficheiro: scrip.js
+	Usado: 
+*/
+
+var gastoHoxe;
+gastoHoxe = 10;
+alert(gastoHoxe);
+/* hace que use una pequeña de la ram para crear variables*/
+
+
+var gastoHoxe = 22.22;
+var gastoOnte = 33.33;
+var total = gastoHoxe + gastoOnte;
+alert(total);
+/*cra una variable con el rsultado de la operacion*/
+
+
