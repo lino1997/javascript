013d43f3a5aadf06c400bbcccd1034ee8370c0cc
lino1997 <56599402+lino1997@users.noreply.github.com>
2020-01-09 13:10:42 +0100

    Add files via upload

---
+++ tarefa1/scrip.js
@@ -0,0 +1,22 @@
+/* 
+	primera tarefa
+	Nome do ficheiro: scrip.js
+	Usado: 
+*/
+
+
+alert("hey buenas a todos")
+
+alert("Moi boas a todos");
+/*funcina copia y pega lo que va entre comillas*/
+
+alert("2+2");
+/*funcina  copiar y pegar numeros y muestra la operacion*/
+
+alert(2+2);
+/*funciona y sale el resultado*/
+alert(-2+3);
+/*funcina sumar numeros negativos con positivo*/
+
+alert(1/3);
+/*funciona dividir en javascrip*/
