990bfc5bbfaa1cf04b7aa70b2c907e096a2db03a
lino1997 <56599402+lino1997@users.noreply.github.com>
2020-01-10 17:12:54 +0100

    Add files via upload

---
+++ Tarefa3/scrip.js
@@ -0,0 +1,20 @@
+/* 
+	Código en JavaScript
+	Nome do ficheiro: scrip.js
+	Usado: 
+*/
+
+var patata=5.3451;
+var cuadrado=Math.pow(patata, 2);
+alert(cuadrado)
+
+var hello = 7.6743 ;
+var raiz = Math.sqrt(hello);
+alert(raiz);
+
+
+var vp = Math.PI;
+alert(vp);
+
+var r = Math.round(vp);
+alert(r);
