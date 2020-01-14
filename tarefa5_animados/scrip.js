ab8b5f8dba7f8258436d523e19f0fd0982ef79d0
lino1997 <56599402+lino1997@users.noreply.github.com>
2020-01-14 17:23:59 +0100

    Add files via upload

---
+++ tarefa5_animados/scrip.js
@@ -0,0 +1,12 @@
+/* 
+	Código en JavaScript
+	Nome do ficheiro: scrip.js
+	Usado: 
+*/
+var nome;
+nome = prompt("Como te chamas?   ");
+alert("Guau... que nome máis interesante " + nome);
+alert("Ti es unha grande persoa " + nome);
+alert("Vales moito " + nome);
+alert("Non hai ninguén máis elegante ca " + nome);
+alert("e... " + nome + ", o mundo adórate!");
