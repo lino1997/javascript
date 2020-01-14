35c5d10b2fa57101c822a82cc78770f2c7ad38ea
lino1997 <56599402+lino1997@users.noreply.github.com>
2020-01-14 16:57:06 +0100

    Add files via upload

---
+++ Tarefa4/scrip.js
@@ -0,0 +1,20 @@
+/* 
+	Código en JavaScript
+	Nome do ficheiro: scrip.js
+	Usado: 
+*/
+
+var euSon = "Profe" + " que " + " conta " + " JavaScript ";
+alert(euSon);
+
+var oficio = "Profe";
+var longura = oficio.length;
+alert(longura);
+
+var oficio = "Profe";
+var rebanada = oficio.slice(3,5);
+alert(rebanada);
+
+var patata="Profe";
+var pato= patata.indexOf("f");
+alert(pato);
