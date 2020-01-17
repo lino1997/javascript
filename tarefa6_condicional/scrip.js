96a59e5339f4755e381911c5b20ccdef82db3d0d
lino1997 <56599402+lino1997@users.noreply.github.com>
2020-01-17 17:54:37 +0100

    Add files via upload

---
+++ tarefa6_condicional/scrip.js
@@ -0,0 +1,18 @@
+
+
+var posicion = 3;
+
+if(posicion == 1) {
+	alert("Medalla de ouro");
+} else { 
+	if(posicion==2){
+		alert("Medalla de plata");
+	} else { 
+	  if(posicion==3){
+		alert("Medalla de bronce");
+	}
+	}
+}
+
+
+
